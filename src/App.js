bb6d882a45c7e5a899a63a2f71255a262102cb61
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-06-03 11:42:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button} from 'semantic-ui-react'
 import './App.css';
+import logo from './logo.png'
+import Projects from './Projects'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="Logo"  className='logo'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Elizabeth 'Biz' Silverstein
         </p>
+        <Button className="ui button">Click Here</Button>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/BabbleBiz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub: babblebiz
         </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/elizabethsilverstein/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn: elizabethsilverstein
+        </a>
+
       </header>
+      <div>
+        <Projects />
+      </div>
     </div>
   );
 }
