ee58e2c3ae8d6a48e2c6b1c7e75e944e30fb97b1
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-06-04 09:49:16 -0500

    Updates

---
+++ precache-manifest.c41af1a2a3854c0a1a6fcaa0e4fb79a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1711f38ecaf01dc96562667393fcc706",
+    "url": "/bizwebsite/index.html"
+  },
+  {
+    "revision": "ddea023a5d61fc2866fe",
+    "url": "/bizwebsite/static/css/main.be16c7a2.chunk.css"
+  },
+  {
+    "revision": "25f29d2e942fc3adb4e6",
+    "url": "/bizwebsite/static/js/2.d09b13fd.chunk.js"
+  },
+  {
+    "revision": "ddea023a5d61fc2866fe",
+    "url": "/bizwebsite/static/js/main.b5c4d3f9.chunk.js"
+  },
+  {
+    "revision": "8ecb4ab0d94a417f5fc4",
+    "url": "/bizwebsite/static/js/runtime~main.962925dc.js"
+  },
+  {
+    "revision": "b6dfcedd2691cba7ea31b42be4629b80",
+    "url": "/bizwebsite/static/media/Headshot.b6dfcedd.jpeg"
+  },
+  {
+    "revision": "6fa6d53621cf2c96967456fa2e29dd02",
+    "url": "/bizwebsite/static/media/energyviz.6fa6d536.png"
+  },
+  {
+    "revision": "5cdeb065f9dfc95ae6cfee3ac9725350",
+    "url": "/bizwebsite/static/media/gracehopper.5cdeb065.png"
+  },
+  {
+    "revision": "56cb6accbd462f111c8f7e00901dd0a9",
+    "url": "/bizwebsite/static/media/pixalive.56cb6acc.png"
+  }
+]);
